184d1d452ae9ec1d1dd783583a8c907e4b9ddd0d
seojin1207 <kama3151515@gmail.com>
2025-11-13 00:19:02 +0900

    20251112_02
    숫자, 사칙연산자, 변수, 복합 대입 연산자 학습 내용

---
+++ 2025111202.py
@@ -0,0 +1,55 @@
+#숫자 20251112
+#정수(int)와 소수(float)
+
+print("사칙 연산자")
+print("5 + 8 =", 5 + 8)
+print("5 - 8 =", 5 - 8)
+print("5 * 8 =", 5 * 8)
+print("5 / 8 =", 5 / 8)
+print("정수 나누기 // = 소수부분이 없어지고 정수만 출력")
+print("5 // 8 =", 5 // 8)
+print("나머지 연산")
+print("8 % 5 =", 8 % 5)
+print("제곱 연산")
+print("2 ** 3 = ", 2 ** 3)
+print()
+#우선순위 (곱셈/나눗셈) > (덧셈/뺄셈) 같은 우선순위는 왼>오
+#먼저 연산하고자 하는 부분은 ()로 묶음
+print((5+3) * 2)
+print(5 + 3 * 2)
+print("\n\n")
+print("변수는 값을 저장하는 식별자")
+print("변수명 = 값 #변수 선언과 저장을 동시")
+pi = 3.14159265 #pi 라는 변수에 3.14159265라는 값을 저장
+r = 5
+print("pi = ", pi)
+print("r = ", r)
+print("원의 넓이 = ", pi * r * r)
+print("\n복합 대입 연산자 (+=, -=, *=, /= 등)")
+number = 100
+print(number)
+number += 10
+print(number)
+number -= 10
+print(number)
+number *= 10
+print(number)
+number /= 10
+print(number)
+#문자열에도 사용가능
+str1 = "안녕하세요"
+print("\n",str1)
+str1 += "~~~!!"
+print(str1)
+
+
+
+
+
+
+
+
+
+
+
+
